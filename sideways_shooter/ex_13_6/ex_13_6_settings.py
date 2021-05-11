531e7dfe3e1f6dff36894475da61247039aa19cc
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-11 13:45:37 -0600

    First commit. New exercise.

---
+++ sideways_shooter/ex_13_6/ex_13_6_settings.py
@@ -0,0 +1,25 @@
+class Settings:
+    """A class to store all the settings for Sideways Shooter."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings.
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Ship settings.
+        self.ship_speed = 1.5
+
+        # Bullet settings.
+        self.bullet_speed = 3.0
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = (255, 0, 0)
+        self.bullets_allowed = 33
+
+        # Alien settings
+        self.alien_speed = 2
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents up, -1 represents down.
+        self.fleet_direction = 1
