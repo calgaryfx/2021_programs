0a80382613891fc5e5a0ae06414a13294ef0a2a7
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-11 14:00:24 -0600

    GameStats Class created for Sideways Shooter.

---
+++ sideways_shooter/ex_13_6/ex_13_6_game_stats.py
@@ -0,0 +1,13 @@
+# Sideways Shooter pt.2. Bring this up to Alien Invasion level. Sideways movement, etc.
+class GameStats:
+    """Track statistics for Sideways Shooter."""
+
+    def __init__(self, ss_game):
+        """Initialize statistics."""
+        self.settings = ss_game.settings
+        self.reset_stats()
+
+    def reset_stats(self):
+        """Initialize statistics taht can change during the game."""
+        self.ships_left = self.settings.ship_limit
+        
