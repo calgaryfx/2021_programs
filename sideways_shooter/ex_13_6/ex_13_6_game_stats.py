5832d50efe252ecac612e3c2a5194fe09800cec0
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-11 14:30:38 -0600

    Add game_active flag so game can end.

--- sideways_shooter/ex_13_6/ex_13_6_game_stats.py
+++ sideways_shooter/ex_13_6/ex_13_6_game_stats.py
@@ -7,7 +7,9 @@ class GameStats:
         self.settings = ss_game.settings
         self.reset_stats()
 
+        # Start Sideways Shooter in an active state.
+        self.game_active = True 
+
     def reset_stats(self):
         """Initialize statistics taht can change during the game."""
         self.ships_left = self.settings.ship_limit
-        
