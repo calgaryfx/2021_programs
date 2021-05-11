2a9673a929fba44d867ba814a75bfb04abda224e
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-11 15:04:24 -0600

    Create _prep_msg()

--- alien_invasion/button.py
+++ alien_invasion/button.py
@@ -18,5 +18,13 @@ class Button:
         self.rect.center = self.screen_rect.center
 
         # The button message needs to be prepped only once.
-        self._pre_msg(msg)
-        
+        self._prep_msg(msg)
+
+    def _prep_msg(self, msg):
+        """Turn msg into a rendered image and center text on the button."""
+        self.msg_image = self.font.render(msg, True, self.text_color,
+                            self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+    
