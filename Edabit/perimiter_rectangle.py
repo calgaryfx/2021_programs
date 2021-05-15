6d6d97c6681c351f459bf52110f06f340c9e6839
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-15 13:10:11 -0600

    First commit. Complete.

---
+++ Edabit/perimiter_rectangle.py
@@ -0,0 +1,8 @@
+# Create a function that takes length and width, calculate the perimiter of a
+# rectangle.
+def find_perimiter(length, width):
+    perimiter = (length + width) * 2
+    return perimiter
+
+a = find_perimiter(20, 10)
+print(a)
