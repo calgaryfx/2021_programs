037fba86b93e582314ad85e88a57be264fef0c11
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-15 13:11:03 -0600

    First commit. Complete.

---
+++ Edabit/return_next_number.py
@@ -0,0 +1,8 @@
+# Create a function that takes a number as an argument, increments the number by
+# + 1 and returns the result.
+def addition(num):
+    plus_one = num + 1
+    return plus_one
+
+number = addition(3)
+print(number)
