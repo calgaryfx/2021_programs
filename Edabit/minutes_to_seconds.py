14af44941952800ef909cba1f5b026e37b2f6023
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-15 13:09:42 -0600

    First commit. Complete.

---
+++ Edabit/minutes_to_seconds.py
@@ -0,0 +1,7 @@
+# Write a function that takes an integer minutes and converts it to seconds.
+def convert(minutes):
+    seconds = minutes * 60
+    return seconds
+
+minutes = convert(60)
+print(minutes)
