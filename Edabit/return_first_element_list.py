512de516ccc37577e2f2d6add3502fafb050eb05
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-15 15:05:10 -0600

    First commit. Complete.

---
+++ Edabit/return_first_element_list.py
@@ -0,0 +1,8 @@
+# Create a function that takes a list containing only numbers and return the
+# first element.
+def get_first_value(number_list):
+    number = (number_list[0])
+    return number
+
+a = get_first_value([-500, 5, 100])
+print(a)
