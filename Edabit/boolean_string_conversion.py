403c574bdbac7ebb63a30ab23bf99bfc4f76e9e3
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-15 14:44:59 -0600

    First commit. Complete

---
+++ Edabit/boolean_string_conversion.py
@@ -0,0 +1,8 @@
+# Create a function that takes a boolean variable flage and returns it as a string.
+def bool_to_string(flag):
+    string = str(flag)
+    return string
+
+st = bool_to_string(True)
+print(st)
+print(st + st)
