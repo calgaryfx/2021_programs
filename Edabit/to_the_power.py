ad93422369403e9e7dddf902a3044cf8e7fbf412
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-15 14:45:48 -0600

    First commit. Complete.

---
+++ Edabit/to_the_power.py
@@ -0,0 +1,8 @@
+# Create a function that takes a base number and an exponent number and returns
+# the calculation.
+def calculate_exponent(num, exp):
+    number = num ** exp
+    return number
+
+n = calculate_exponent(3, 3)
+print(n)
