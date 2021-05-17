2571a5bcaddb9492c4a2f4fcbd98a4eface1157c
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-17 14:35:44 -0600

    Button length shortened for appearance.

--- sideways_shooter/ex_14_1/ex_14_1_button.py
+++ sideways_shooter/ex_14_1/ex_14_1_button.py
@@ -8,7 +8,7 @@ class Button:
         self.screen_rect = self.screen.get_rect()
 
         # Set the dimensions and properties of the button.
-        self.width, self.height = 1000, 50
+        self.width, self.height = 300, 50
         self.button_color = (255, 0, 0)
         self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont(None, 48)
