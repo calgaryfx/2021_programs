e319d322f908e96a7e3c148ed41616865fa9bdbf
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-17 10:26:50 -0600

    Hide mouse cursor during gameplay. Bring back at game end.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -75,6 +75,9 @@ class AlienInvasion:
             self._create_fleet()
             self.ship.center_ship()
 
+            # Hide the mouse cursor.
+            pygame.mouse.set_visible(False)
+
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
         if event.key == pygame.K_RIGHT:
@@ -209,6 +212,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
