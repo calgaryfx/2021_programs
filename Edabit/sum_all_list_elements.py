8f9b2b2f09abec580bf7e2df614ef4dc2fa58faf
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-20 15:14:21 -0600

    First commit. Complete.

---
+++ Edabit/sum_all_list_elements.py
@@ -0,0 +1,13 @@
+# Create a function that takes a list and returns the sum of all numbers in the
+# list.
+def get_sum_of_elements(lst):
+    return sum(lst)
+
+a = get_sum_of_elements([2, 7, 4])
+print(a)
+
+a = get_sum_of_elements([45, 3, 0])
+print(a)
+
+a = get_sum_of_elements([-2, 84, 23])
+print(a)
