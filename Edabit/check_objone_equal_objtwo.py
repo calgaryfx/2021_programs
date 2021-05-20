3a284dcfc12d94d9fc53626ca2cbf4c8a53d84f6
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-20 14:07:03 -0600

    Revised with shorter code. One line.

--- Edabit/check_objone_equal_objtwo.py
+++ Edabit/check_objone_equal_objtwo.py
@@ -1,10 +1,7 @@
 # Create a function that checks to see if two object arguments are equal to one
 # another. Return True if the objects are equal, otherwise, return False.
 def is_equal(obj_one, obj_two):
-    if obj_one == obj_two:
-        return True
-    else:
-        return False
+    return obj_one == obj_two
 
 obj_one = {
     "name": "Benny",
