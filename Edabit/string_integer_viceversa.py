5dd85611f5a17515bf296e06de67fb4f763d0fc0
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-20 09:01:28 -0600

    First commit. Challenge.

---
+++ Edabit/string_integer_viceversa.py
@@ -0,0 +1,8 @@
+# Write two functions:
+# 1. 'to_int():' A function to convert a string to an integer.
+# 2. 'to_str():' A function to convert a integer to a string.
+def to_int(txt):
+
+
+def to_str(num):
+    
