ccf73615d5576402acdb5562474307bee314ed7c
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-20 11:50:54 -0600

    First commit. Complete.

---
+++ Edabit/find_largest_number_list.py
@@ -0,0 +1,13 @@
+# Create a function that takes a list of numbers. Return the largest number in
+# the list.
+def findLargestNum(nums):
+    return max(nums)
+
+a = findLargestNum([4, 5, 1, 3])
+print(a)
+
+a = findLargestNum([300, 200, 600, 150])
+print(a)
+
+a = findLargestNum([1000, 1001, 857, 1])
+print(a)
