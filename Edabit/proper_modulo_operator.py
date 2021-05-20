c09e352a87fded80e554d8cf0e54895acc4a60a5
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-20 11:24:58 -0600

    First commit. Complete.

---
+++ Edabit/proper_modulo_operator.py
@@ -0,0 +1,12 @@
+# Create a function which returns the Modulo of the two given numbers.
+def mod(m, n):
+    return m % n
+
+remainder = mod(-13, 64)
+print(remainder)
+
+remainder = mod(50, 25)
+print(remainder)
+
+remainder = mod(13, 3)
+print(remainder)
