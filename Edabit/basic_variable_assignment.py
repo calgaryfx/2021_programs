8929b4e38cfe65094a608648618353778fe7d1d9
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-20 08:40:51 -0600

    First commit. Challenge.

---
+++ Edabit/basic_variable_assignment.py
@@ -0,0 +1,6 @@
+# Code should concatenate a passed string 'name' with string 'Edabit' and store
+# it in a vairable called 'result'. Fix the code.
+def name_string(name):
+    b == "Edabit"
+    result == name + b
+    return result
