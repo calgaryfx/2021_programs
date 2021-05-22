cea12b9479ac3857468e96f63c5cb2f530f6214a
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-22 12:57:55 -0600

    Changed to use a dictionary. Cleaner now.

--- Edabit/luke_i_am.py
+++ Edabit/luke_i_am.py
@@ -1,16 +1,14 @@
 # Luke Skywalker has family and friends. Help him remind them who is who. Given
 # a string with a name, return the relation of that person to Luke.
 def relation_to_luke(name):
-    relation = "Luke, I am your "
+    relation = {
+        "Darth Vader": "father",
+        "Leia": "sister",
+        "Han": "brother in law",
+        "R2D2": "droid",
+        }
 
-    if name == "Darth Vader":
-        return (relation + "father.")
-    elif name == "Leia":
-        return (relation + "sister.")
-    elif name == "Han":
-        return (relation + "brother in law.")
-    elif name == "R2D2":
-        return (relation + "droid.")
+    return f"Luke, I am your {relation[name]}."
 
 x = relation_to_luke("Darth Vader")
 print(x)
