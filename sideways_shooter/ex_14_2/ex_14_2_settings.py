f7d1664835429c77aac5805a51c22a06c3bd261c
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-25 08:40:24 -0600

    First commit.

---
+++ sideways_shooter/ex_14_2/ex_14_2_settings.py
@@ -0,0 +1,16 @@
+# PCC (P.285) Ex.14-2 Target Practice.
+# Create ship on right side that moves up and down at a asteady rate.
+# Create ship on left side player can move up and down and fire bullets.
+# Add a play button that starts the game, after the player misses the target three
+# times, end the game, have play button re-appear to re-start.
+import pygame
+
+from ex_14_2_settings import Settings 
+
+class SidewaysShooter:
+    """Overall class to manage game assets and behavior."""
+
+    def _init_(self):
+        """Initialize the game, create game resources."""
+        pygame.init()
+        self.setting = Settings()
