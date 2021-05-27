c4b7f6dac192b5933fe6b70887927e27cc9ad382
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-27 16:07:37 -0600

    Add if statement to while loop in run_game()

--- sideways_shooter/ex_14_2/ex_14_2.py
+++ sideways_shooter/ex_14_2/ex_14_2.py
@@ -27,10 +27,15 @@ class TargetPractice:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Target Practice")
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
+
+            if self.stats.game_active:
+                self.ship.blitme()
 
             self._update_screen()
 
