93f7c04d531fd8f03ed0e99b636a8de18969cccd
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-27 14:50:33 -0600

    First commit.

---
+++ sideways_shooter/ex_14_2/ex_14_2_game_stats.py
@@ -0,0 +1,7 @@
+# PCC (P.285) Ex.14-2 Target Practice.
+class GameStats:
+    """Track statistics for Sideways Shooter."""
+
+    def __init__(self, tp_game):
+        """Initialize statistics."""
+    
