c2af385a47b74b53f04b84016e2e987e0b9d1d63
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-27 15:45:25 -0600

    Initialize ships starting postion, image location, create bliteme()

--- sideways_shooter/ex_14_2/ex_14_2_ship.py
+++ sideways_shooter/ex_14_2/ex_14_2_ship.py
@@ -6,4 +6,19 @@ class Ship:
 
     def __init__(self, tp_game):
         """Initialize the ship and set its starting position."""
-        
+        self.screen = tp_game.screen
+        self.settings = tp_game.settings
+        self.screen_rect = tp_game.screen.get_rect()
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('images/rocket_small.png')
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the middle left of the screen.
+        self.rect.midleft = self.screen_rect.midleft
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+    
