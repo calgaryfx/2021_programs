513be86d87347f61d8c183fa848d710c31e5bb22
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-27 14:51:06 -0600

    First commit.

---
+++ sideways_shooter/ex_14_2/ex_14_2_ship.py
@@ -0,0 +1,9 @@
+# PCC (P.285) Ex.14-2 Target Practice.
+import pygame
+
+class Ship:
+    """A class to manage the ship on the left side of the screen."""
+
+    def __init__(self, tp_game):
+        """Initialize the ship and set its starting position."""
+        
