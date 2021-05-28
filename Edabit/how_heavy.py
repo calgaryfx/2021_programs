2ba3e91b1936db366bfafdaa231c0493db5117c5
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-28 11:24:28 -0600

    First commit. Complete.

---
+++ Edabit/how_heavy.py
@@ -0,0 +1,15 @@
+# Given radius r and height h (in cm), calcualte the mass of a cylinder when it
+# is filled with water and the cylinder itself doesn't weigh anything. The
+# desired output should be given in kg and rounded to two decimal places.
+import math
+def weight(r, h):
+    return round(((math.pi * (r ** 2)) * h) * 0.001, 2)
+
+a = weight(4, 10)
+print(a)
+
+a = weight(30, 60)
+print(a)
+
+a = weight(15, 10)
+print(a)
