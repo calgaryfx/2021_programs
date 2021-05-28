602ce1fc91969341bfec25f42d612d7959c31ebe
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-28 13:27:13 -0600

    First commit. Complete.

---
+++ Edabit/milk_cookies.py
@@ -0,0 +1,21 @@
+# Xmas Eve is almost upon us, prepare milk and cookies for Santa. Create a function
+# that accepts a Date object and returns True if it's Xmas Eve (Dec. 24) and False
+# otherwise.
+import datetime
+def time_for_milk_and_cookies(date):
+    if date.month == 12 and date.day == 24:
+        return True
+    else:
+        return False
+
+a = time_for_milk_and_cookies(datetime.date(2013, 12, 24))
+print(a)
+
+a = time_for_milk_and_cookies(datetime.date(2013, 1, 23))
+print(a)
+
+a = time_for_milk_and_cookies(datetime.date(3000, 12, 24))
+print(a)
+
+a = time_for_milk_and_cookies(datetime.date(2023, 12, 5))
+print(a)
