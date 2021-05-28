d35ab021f696728914aebef5fd9acf7a5fe2dd69
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-28 11:25:44 -0600

    Updated to only have 'pi' in return statement.

--- Edabit/how_heavy.py
+++ Edabit/how_heavy.py
@@ -1,9 +1,9 @@
 # Given radius r and height h (in cm), calcualte the mass of a cylinder when it
 # is filled with water and the cylinder itself doesn't weigh anything. The
 # desired output should be given in kg and rounded to two decimal places.
-import math
+from math import pi
 def weight(r, h):
-    return round(((math.pi * (r ** 2)) * h) * 0.001, 2)
+    return round(((pi * (r ** 2)) * h) * 0.001, 2)
 
 a = weight(4, 10)
 print(a)
