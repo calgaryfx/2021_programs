b6e9a45aadacb6b87d443158059c5b708aeb5e37
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-06-02 13:27:37 -0600

    Create self.y attribute to hold decimal values for ship speed.

--- sideways_shooter/ex_14_2/ex_14_2_ship.py
+++ sideways_shooter/ex_14_2/ex_14_2_ship.py
@@ -7,6 +7,7 @@ class Ship:
     def __init__(self, tp_game):
         """Initialize the ship and set its starting position."""
         self.screen = tp_game.screen
+        self.settings = tp_game.settings
         self.screen_rect = tp_game.screen.get_rect()
 
         # Load the ship image and get its rect.
@@ -16,16 +17,23 @@ class Ship:
         # Start each new ship at the left side/center of the screen.
         self.rect.midleft = self.screen_rect.midleft
 
+        # Store a decimal value for the ships vertical position.
+        self.y = float(self.rect.y)
+
         # Movement flag
         self.moving_up = False
         self.moving_down = False
 
     def update(self):
-        """Update the ship's position based on the movement flag."""
+        """Update the ship's position based on the movement flags."""
+        # Update the ships y value, not the rect.
         if self.moving_up:
-            self.rect.y -= 3
+            self.y -= self.settings.ship_speed
         if self.moving_down:
-            self.rect.y += 3
+            self.y += self.settings.ship_speed
+
+        # Update rect object from self.y.
+        self.rect.y = self.y
 
     def blitme(self):
         """Draw the ship at its current location."""
