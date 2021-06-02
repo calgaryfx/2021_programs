8a0beaff9af938fd5d690873de99ff7b335fffd5
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-06-02 12:39:15 -0600

    Move check events from run_game() method and create _check_events()

--- sideways_shooter/ex_14_2/ex_14_2.py
+++ sideways_shooter/ex_14_2/ex_14_2.py
@@ -27,10 +27,7 @@ class TargetPractice:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
 
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
@@ -38,6 +35,13 @@ class TargetPractice:
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
 
 if __name__ == '__main__':
     # Make a game instance, run the game.
