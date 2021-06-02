c3ccb651f64a0f0752a2b712ac7cbd11904db3ef
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-06-02 11:48:11 -0600

    Fresh Start. First commit. Settings class created.

--- sideways_shooter/ex_14_2/ex_14_2_settings.py
+++ sideways_shooter/ex_14_2/ex_14_2_settings.py
@@ -1,13 +1,10 @@
 # PCC (P.285) Ex.14-2 Target Practice.
 class Settings:
-    """Sideways Shooter settings."""
+    """Target Practice settings."""
 
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings.
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
-
-        # Ship settings.
-        
+        self.bg_color = (0, 0, 0)
