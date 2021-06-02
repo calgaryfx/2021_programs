8f141f29e386a7a36dc64e6d9731c9dc81e38996
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-06-02 14:05:14 -0600

    Fresh commit. Create Bullet class to fire bullets from ship across the screen.

--- sideways_shooter/ex_14_2/ex_14_2_bullet.py
+++ sideways_shooter/ex_14_2/ex_14_2_bullet.py
@@ -7,4 +7,27 @@ class Bullet(Sprite):
 
     def __init__(self, tp_game):
         """Create a bullet objective at the ships current position."""
+        super().__init__()
+        self.screen = tp_game.screen
+        self.setting = tp_game.settings
+        self.color = self.settings.bullet_color
+
+        # Create a bullet rect at (0, 0) and then set correct position.
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
+            self.settings.bullet_height)
+        self.rect.midright = tp_game.ship.rect.midright
+
+        # Store the bullets position as a decimal value.
+        self.x = float(self.rect.x)
+
+    def update(self):
+        """Move the bullet across the screen."""
+        # Update the decimal position of the bullet.
+        self.x += self.settings.bullet_speed
+        # Update the rect position.
+        self.rect.x = self.x
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
         
