b18c12555f6254871a7efbf8385715892269d1f6
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-06-02 13:12:22 -0600

    Modify _check_events() to add K_DOWN to KEYUP and KEYDOWN events.

--- sideways_shooter/ex_14_2/ex_14_2.py
+++ sideways_shooter/ex_14_2/ex_14_2.py
@@ -39,9 +39,13 @@ class TargetPractice:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     self.ship.moving_up = True
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_UP:
                     self.ship.moving_up = False
+                elif event.key == pygame.K_DOWN:
+                    self.ship.moving_down = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
