b346f65fc36a97894355c5c9e9b8623d508469f2
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-06-02 12:45:56 -0600

    Move update screen code to _update_screen() from run_game() method.

--- sideways_shooter/ex_14_2/ex_14_2.py
+++ sideways_shooter/ex_14_2/ex_14_2.py
@@ -28,13 +28,7 @@ class TargetPractice:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()  # Ship is drawn after background to not disappear.
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._update_screen()
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -42,6 +36,13 @@ class TargetPractice:
             if event.type == pygame.QUIT:
                 sys.exit()
 
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()  # Ship is drawn after background to not disappear.
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, run the game.
