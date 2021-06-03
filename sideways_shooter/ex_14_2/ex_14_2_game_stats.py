37635476818b6b120ade27bcf95b6f59a3633143
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-06-03 14:12:41 -0600

    Set stats for alien ship limit.

--- sideways_shooter/ex_14_2/ex_14_2_game_stats.py
+++ sideways_shooter/ex_14_2/ex_14_2_game_stats.py
@@ -7,9 +7,6 @@ class GameStats:
         self.settings = tp_game.settings
         self.reset_stats()
 
-        # Start Target Practice in an active state.
-        self.game_active = True
-
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
-        self.ships_left = self.settings.ship_limit
+        self.aliens_left = self.settings.alien_limit
