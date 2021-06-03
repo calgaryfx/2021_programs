9047ceaf4b1e61919855e903c2de5ad6f0973853
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-06-03 12:25:49 -0600

    Add screen_rect attribute to set alien position. Line 16-18

--- sideways_shooter/ex_14_2/ex_14_2_alien.py
+++ sideways_shooter/ex_14_2/ex_14_2_alien.py
@@ -6,3 +6,16 @@ class Alien(Sprite):
     """A class to represent the alien ship."""
     def __init__(self, tp_game):
         """Initialize the alien and set its starting position."""
+        super().__init__()
+        self.screen = tp_game.screen
+
+        # Load the alien ship image and set its rect attribute.
+        self.image = pygame.image.load('images/alien_ship.png')
+        self.rect = self.image.get_rect()
+
+        # Start each new alien near the top right of the screen.
+        self.screen_rect = self.screen.get_rect()
+        self.rect.midright = self.screen_rect.midright
+
+        # Store the alien's exact horizontal position.
+        self.x = float(self.rect.x)
