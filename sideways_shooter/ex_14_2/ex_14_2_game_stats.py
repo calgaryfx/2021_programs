c715c9cfe2c2dfad9d73fa867dfe590824da8604
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-06-03 14:18:14 -0600

    Add game active state.

--- sideways_shooter/ex_14_2/ex_14_2_game_stats.py
+++ sideways_shooter/ex_14_2/ex_14_2_game_stats.py
@@ -7,6 +7,9 @@ class GameStats:
         self.settings = tp_game.settings
         self.reset_stats()
 
+        # Start Target Practice in an active state.
+        self.game_active = True 
+
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.aliens_left = self.settings.alien_limit
