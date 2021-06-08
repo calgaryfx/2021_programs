24eb14f2940408cba46cc16e974d402b8ca94199
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-06-08 10:30:45 -0600

    Update rectangle length for larger start game message, change color of rectangle to red with black text.

--- sideways_shooter/ex_14_2/ex_14_2_button.py
+++ sideways_shooter/ex_14_2/ex_14_2_button.py
@@ -9,9 +9,9 @@ class Button:
         self.screen_rect = self.screen.get_rect()
 
         # Set the dimensions and properties of the button.
-        self.width, self.height = 200, 50
-        self.button_color = (0, 255, 0)
-        self.text_color = (255, 255, 255)
+        self.width, self.height = 400, 50
+        self.button_color = (255, 0, 0)
+        self.text_color = (0, 0, 0)
         self.font = pygame.font.SysFont(None, 48)
 
         # Build the buttons rect object and center it.
