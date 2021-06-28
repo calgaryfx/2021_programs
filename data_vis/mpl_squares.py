53179103365c363bece18c5f562f581a6a776cd5
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-06-28 14:07:21 -0600

    Built-in styles added, used fivethirtyeight style at Ln.6

--- data_vis/mpl_squares.py
+++ data_vis/mpl_squares.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
+plt.style.use('fivethirtyeight')
 fig, ax = plt.subplots()
 ax.plot(input_values, squares, linewidth=3)
 
@@ -15,3 +16,32 @@ ax.set_ylabel("Square of Value", fontsize=14)
 ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
+
+
+# Built-in styles that can be used for graph. Add code before generating plot:
+#Solarize_Light2
+#_classic_test_patch
+#bmh
+#classic
+#dark_background
+#fast
+#fivethirtyeight
+#ggplot
+#grayscale
+#seaborn
+#seaborn-bright
+#seaborn-colorblind
+#seaborn-dark
+#seaborn-dark-palette
+#seaborn-darkgrid
+#seaborn-deep
+#seaborn-muted
+#seaborn-notebook
+#seaborn-paper
+#seaborn-pastel
+#seaborn-poster
+#seaborn-talk
+#seaborn-ticks
+#seaborn-white
+#seaborn-whitegrid
+#tableau-colorblind10
