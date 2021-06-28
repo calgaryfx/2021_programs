d8974fa1a7af088a57614f02ecac233a758b1d1a
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-06-28 11:00:41 -0600

    Add self.score_scale and print statment to confirm score increases for each alien for each new level.

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -25,6 +25,9 @@ class Settings:
         # How quickly the game speeds up
         self.speedup_scale = 1.3
 
+        # How quickly the alien point values increase.
+        self.score_scale = 1.5
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -40,3 +43,6 @@ class Settings:
     def increase_speed(self):
         """Increase speed settings."""
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        #print(self.alien_points)
