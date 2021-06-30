bedadf87ad3c7727ad6b7bd5598692c6a7710fad
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-06-30 12:09:16 -0600

    zorder added to make start and end points lay on top of lines. Not taught in book, only found in authors solutions. Nice job author.

--- data_vis/ex_15_3_molecular_motion.py
+++ data_vis/ex_15_3_molecular_motion.py
@@ -10,14 +10,11 @@ rw.fill_walk()
 plt.style.use('classic')
 fig, ax = plt.subplots(figsize=(16, 9.6), dpi=128)
 point_numbers = range(rw.num_points)
-ax.plot(rw.x_values, rw.y_values, linewidth=1)  # Had to look at authors solution.
+ax.plot(rw.x_values, rw.y_values, linewidth=1, zorder=1)
 
 # Emphasize the first and last points of the walk.
-ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+ax.scatter(0, 0, c='green', edgecolors='none', s=100, zorder=2)
 ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
-    s=100)
+    s=100, zorder=2)
 
 plt.show()
-
-# Line 13 I could not have figured out without a previous example. Had to see
-# authors solution. Nice assignment without teaching the info!
