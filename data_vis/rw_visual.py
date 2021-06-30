3a4d8923869cc6289b9d54bf4de3a24adff47084
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-06-30 11:34:32 -0600

    Change screen size to fit display.

--- data_vis/rw_visual.py
+++ data_vis/rw_visual.py
@@ -10,19 +10,15 @@ while True:
 
     # Plot the points in the walk.
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(16, 9.6), dpi=128)
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-        edgecolors='none', s=1)
+        edgecolors='none', s=10)
 
     # Emphasize the first and last points of the walk.
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
         s=100)
-
-    # Remove the axes.
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
 
     plt.show()
 
