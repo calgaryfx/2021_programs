f90956dbff50c00ed183610880a246e22b73119d
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-07-12 14:02:07 -0600

    Reduce size of first and last points of the walk by half.

--- data_vis/ex_15_5_refactoring.py
+++ data_vis/ex_15_5_refactoring.py
@@ -14,8 +14,8 @@ ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
     edgecolors='none', s=10)
 
 # Emphasize the first and last points of the walk.
-ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+ax.scatter(0, 0, c='green', edgecolors='none', s=50)
 ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
-    s=100)
+    s=50)
 
 plt.show()
