5a25344068c77fc03cbaa70796191fb4c2d04c69
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-07-12 13:30:17 -0600

    First commit for new exercise.

---
+++ data_vis/ex_15_5_refactoring.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+from ex_15_5_random_walk import RandomWalk
+
+# Make a random walk.
+rw = RandomWalk(5_000)
+rw.fill_walk()
+
+# Plot the points in the walk.
+plt.style.use('classic')
+fig, ax = plt.subplots(figsize=(16, 9.6), dpi=128)
+point_numbers = range(rw.num_points)
+ax.plot(rw.x_values, rw.y_values, linewidth=1, zorder=1)
+
+# Emphasize the first and last points of the walk.
+ax.scatter(0, 0, c='green', edgecolors='none', s=100, zorder=2)
+ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+    s=100, zorder=2)
+
+plt.show()
