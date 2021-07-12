e4512a61643f5010518959faf3bf650d6a825504
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-07-12 14:51:43 -0600

    Call Bar and Layout classes from plotly. Add title, x and y axis descriptions, title, store x & y values, call offline.plot() function, create html file to open in browser and store in program folder.

--- data_vis/die_visual.py
+++ data_vis/die_visual.py
@@ -1,3 +1,6 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
 from die import Die
 
 # Create a D6. D6 being six sided dice. D8 being 8 sided dice.
@@ -14,6 +17,16 @@ frequencies = []
 for value in range(1, die.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
+
+# Visualize the results.
+x_values = list(range(1, die.num_sides+1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(title='Results of rolling one six sided dice 1000 times',
+        xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
 
 print(frequencies)
 print("Number of rolls:", (len(results)))
